f31b96f959203c0e25f1cc961e6c3420e1e72cbf
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-20 14:55:59 -0500

    Opening program files from command line possible.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -27,21 +27,24 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        ##opening a file or program and reading line by line
+        ##need to account for sys.argv[1] being left blank
+        if len(sys.argv) < 2:
+            print('No program specified to be run.')
+            sys.exit()
+        else: 
+            with open(sys.argv[1]) as f:
+                ##read program file line by line
+                for line in f:
+                    ##convert to single lines of non-string binary numbers
+                    string_val = line.split("#")[0].strip()
+                    if string_val == '':
+                        continue
+                    v = int(string_val, 2)
+                    self.ram[address] = v
+                    ##move to next line in program
+                    address += 1
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
 
 
     def alu(self, op, reg_a, reg_b):
