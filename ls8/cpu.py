ded4583fe36d0fec23bcff15708deb7b186b3ce5
Jessica Cooper <jessicagcooper@gmail.com>
2020-05-21 02:04:34 -0500

    stack working with program hardcoded in for debugging.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,8 @@ LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
 MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110
 
 class CPU:
     """Main CPU class."""
@@ -15,12 +17,15 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0 # program counter, the address of the current instruction
-        self.sp = 0xF4 # stack pointer aka R7 of registef\r
+        self.sp = 7 # stack pointer aka R7 of register
+        self.reg[self.sp] = 0xf4
         self.branchtable = {
             LDI: self.ldi,
             PRN: self.prn,
             HLT: self.hlt,
-            MUL: self.mul
+            MUL: self.mul,
+            PUSH: self.push,
+            POP: self.pop
         }        
 
 
@@ -29,25 +34,63 @@ class CPU:
 
         address = 0
 
+        program = [
+            0b10000010, # LDI R0,1
+            0b00000000,
+            0b00000001,
+            0b10000010, # LDI R1,2
+            0b00000001,
+            0b00000010,
+            0b01000101, # PUSH R0
+            0b00000000,
+            0b01000101, # PUSH R1
+            0b00000001,
+            0b10000010, # LDI R0,3
+            0b00000000,
+            0b00000011,
+            0b01000110, # POP R0
+            0b00000000,
+            0b01000111, # PRN R0
+            0b00000000,
+            0b10000010, # LDI R0,4
+            0b00000000,
+            0b00000100,
+            0b01000101, # PUSH R0
+            0b00000000,
+            0b01000110, # POP R2
+            0b00000010,
+            0b01000110, # POP R1
+            0b00000001,
+            0b01000111, # PRN R2
+            0b00000010,
+            0b01000111, # PRN R1
+            0b00000001,
+            0b00000001 # HLT
+        ]
+
+        for instruction in program:
+            self.ram[address] = instruction
+            address += 1
+
         ##opening a file or program and reading line by line
         ##need to account for sys.argv[1] being left blank
         
-        if len(sys.argv) < 2:
-            print('No program specified to be run.')
-            sys.exit()
-        else: 
-            with open(sys.argv[1]) as f:
-                ##read program file line by line
-                for line in f:
-                    ##convert to single lines of non-string binary numbers
-                    string_val = line.split("#")[0].strip()
-                    if string_val == '':
-                        continue
-                    v = int(string_val, 2)
-                    ##set that line of program to current address in ram:
-                    self.ram[address] = v
-                    ##move to next line in program:
-                    address += 1
+        # if len(sys.argv) < 2:
+        #     print('No program specified to be run.')
+        #     sys.exit()
+        # else: 
+        #     with open(sys.argv[1]) as f:
+        #         ##read program file line by line
+        #         for line in f:
+        #             ##convert to single lines of non-string binary numbers
+        #             string_val = line.split("#")[0].strip()
+        #             if string_val == '':
+        #                 continue
+        #             v = int(string_val, 2)
+        #             ##set that line of program to current address in ram:
+        #             self.ram[address] = v
+        #             ##move to next line in program:
+        #             address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -124,3 +167,26 @@ class CPU:
         result = mult1 * mult2
         self.reg[operand_a] = result
         self.pc +=3
+
+    def push(self, operand_a, operand_b):
+        ##decrement stackpointer
+        self.reg[self.sp] -= 1
+        ##value to store is located in register at index operand_a as given
+        val = self.reg[operand_a]
+        ##current top of stack address in ram is value located at address stored at R7 or stack pointer
+        top_of_stack_address = self.reg[self.sp]
+        ##use ram_write function where mar = address to write to, mdr = value to write to address
+        self.ram_write(top_of_stack_address, val)
+        
+        self.pc +=2
+    
+    def pop(self, operand_a, operand_b):
+        ##need to extract value at current sp location
+        val = self.ram_read(self.reg[self.sp])
+        ##need to extract reg address to store to 
+        ##reg_address = self.reg[operand_a]
+        self.reg[operand_a] = val
+        ##increment stackpointer
+        self.reg[self.sp] += 1
+        
+        self.pc +=2
